f9d6777c7c9b8d18d6142ac5787d2b0a75256393
Juan Delgado <Delgado-juan@hotmail.com>
2023-01-06 15:53:38 -0500

    adding my version for pakaage json and app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import "./index.css";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import React from 'react';
+import './index.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-    <Router basename="React-Portfolio">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-    </>
-  );
+    return (
+        <>
+            <Router basename='/practice_Portfolio'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/project' element={<Projects />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
